c4b53232e8aab79a38700f4ee25085ec2e1c721e
funkytek <aaron@wearefractal.com>
2014-08-22 21:41:16 -0700

    clean

--- examples/coffee-require/public/models/Todo.js
+++ examples/coffee-require/public/models/Todo.js
@@ -2,16 +2,11 @@
   define(function(require) {
     var fission;
     fission = require('vendor/fission');
-    console.log(fission);
     return fission.model({
       props: {
         text: 'string'
-<<<<<<< HEAD
       },
       url: '/v1/todos'
-=======
-      }
->>>>>>> upstream/master
     });
   });
 
