39e8e0624ff8f52b5495d0ca881ea5223d88c50d
Contra <contra@maricopa.edu>
2015-01-26 17:10:44 -0800

    start anew

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
 'use strict';
 
-var clone = require('lodash.clone');
 var React = require('react');
 var Router = require('fission-router');
 
-function Fission() {
-  this.options = {};
-}
+module.exports = {
+  router: Router,
+  component: require('./lib/renderables/component'),
+  view: require('./lib/renderables/view'),
+  modelView: require('./lib/renderables/modelView'),
+  collectionView: require('./lib/renderables/collectionView'),
 
-Fission.prototype.setOptions = function(opts) {
-  this.options = opts ? clone(opts) : {};
+  // data stuff
+  model: require('./lib/data/model'),
+  collection: require('./lib/data/collection'),
+
+  // expose underlying react stuff
+  React: React,
+  DOM: React.DOM,
+  createElement: React.createElement,
+  createFactory: React.createFactory,
+  PropTypes: React.PropTypes,
+  render: React.render
 };
-
-Fission.prototype.router = Router;
-Fission.prototype.component = require('./lib/renderables/component');
-Fission.prototype.view = require('./lib/renderables/view');
-Fission.prototype.modelView = require('./lib/renderables/modelView');
-Fission.prototype.collectionView = require('./lib/renderables/collectionView');
-
-Fission.prototype.model = require('./lib/data/model');
-Fission.prototype.createCollection = require('./lib/data/createCollection');
-
-Fission.prototype.mixins = {
-  Listener: require('./lib/mixins/Listener'),
-  Sugar: require('./lib/mixins/Sugar'),
-  State: Router.mixins.State,
-  Navigation: Router.mixins.Navigation
-};
-
-// expose some react stuff
-Fission.prototype.React = React;
-Fission.prototype.DOM = React.DOM;
-Fission.prototype.createElement = React.createElement;
-Fission.prototype.createFactory = React.createFactory;
-Fission.prototype.PropTypes = React.PropTypes;
-Fission.prototype.render = React.render;
-
-module.exports = new Fission();
